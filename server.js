2b38ec5eed072c2322289cfbdb3009f0abd5242e
asanElzhanov <123302636+asanElzhanov@users.noreply.github.com>
2024-01-27 22:22:38 +0600

    Update server.js

--- server.js
+++ server.js
@@ -42,6 +42,27 @@ app.post('/addTour', (req, res) => {
   tours.push(newTour);
 });
 
+app.get('/getWeather', async (req, res) => {
+    try {
+      const apiKey = '5350dd8a37fb4c75827123533242401 ';
+      const city = req.query.city || 'New York';
+  
+      const response = await axios.get(`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`);
+  
+      const weatherData = {
+        location: response.data.location.name,
+        temperature: response.data.current.temp_c,
+        condition: response.data.current.condition.text,
+        windSpeed: response.data.current.wind_mph,
+      };
+  
+      res.json(weatherData);
+    } catch (error) {
+      console.error('Error fetching weather data:', error.message);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
+
 
 // Handle DELETE request to remove a tour
 app.delete('/deleteTour/:id', (req, res) => {
